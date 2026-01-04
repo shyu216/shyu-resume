d892c6dddae82bb85c96c7481ac66270eb9d501c
shyu216 <yusihong073@gmail.com>
2026-01-04 12:26:41 +1100

    up

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
     output: 'export',
     images: { unoptimized: true },
-    basePath: isProd ? '/shyu-resume' : '',
-    assetPrefix: isProd ? '/shyu-resume/' : '',
 };
 
 module.exports = nextConfig;
