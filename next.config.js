0366964aa90d8fb620e095f5be3caa39f485aa50
shyu216 <yusihong073@gmail.com>
2026-01-04 11:51:01 +1100

    up prod path

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
     output: 'export',
-    images: { unoptimized: true } 
-}
+    images: { unoptimized: true },
+    basePath: isProd ? '/shyu-resume' : '',
+    assetPrefix: isProd ? '/shyu-resume/' : '',
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
