522e94222ddb091d6e1012ee0288a053a2f94e57
shyu216 <yusihong073@gmail.com>
2026-03-02 23:22:14 +1100

    fix-pagination

--- components/job/job-stack-keywords.ts
+++ components/job/job-stack-keywords.ts
@@ -1,9 +1,9 @@
 import { JobType } from './job-switcher';
 
 export const jobStackKeywords: Record<JobType, string[]> = {
-  FULLSTACK: ['nextjs', 'react', 'react native', 'typescript', 'javascript', 'node.js', 'aws', 'restful api', 'dynamodb', 'tailwind css'],
-  SOFTWARE: ['node.js', 'go', 'python', 'c#', 'typescript', 'javascript', 'restful api', 'mysql', 'sqlite', 'dynamodb'],
-  ML_RESEARCHER: ['pytorch', 'yolo', 'openpcdet', 'open3d', 'onnx', 'hugging face', 'scikit-learn', 'doc2vec', 'sbert', 'bilstm', 'cuda'],
+  FULLSTACK: ['nextjs', 'react', 'react native', 'typescript', 'javascript', 'node.js', 'aws', 'restful api', 'dynamodb', 'tailwind css', 'html/css', 'ui/3d integration', 'firebase auth', 'socket.io', 'stripe api', 'jwt', 'oauth 2.0', 'git', 'i18n', 'agile/scrum'],
+  SOFTWARE: ['node.js', 'go', 'python', 'c#', 'typescript', 'javascript', 'restful api', 'mysql', 'sqlite', 'dynamodb', 'postgresql', 'postgis', 'hadoop/mapreduce', 'er modelling', 'spatial analysis', 'distributed systems', 'raft', 'slurm', 'linux', 'nginx'],
+  ML_RESEARCHER: ['pytorch', 'yolo', 'openpcdet', 'open3d', 'onnx', 'hugging face', 'scikit-learn', 'doc2vec', 'sbert', 'bilstm', 'cuda', 'python', 'flask', 'opencv', 'numpy', 'evm', 'rppg', 'real sense', 'compute shader', 'unity', 'meta quest 3'],
 };
 
 export const getJobStackKeywords = (jobType: JobType): string[] => {
